cc605419bebad8d42d4deedc1d16627eebb17775
olegshulyakov <olegshulyakov@noreply.github.com>
2016-09-20 19:01:28 +0300

    converted EmptyMinute400EquityAlgorithm to java

---
+++ quantconnect-java/src/main/java/QuantConnect/example/Benchmarks/EmptyMinute400EquityAlgorithm.java
@@ -0,0 +1,22 @@
+﻿package QuantConnect.example.Benchmarks;
+
+import cli.QuantConnect.Algorithm.QCAlgorithm;
+import cli.QuantConnect.Data.Slice;
+import cli.QuantConnect.Resolution;
+import cli.QuantConnect.SecurityType;
+
+public class EmptyMinute400EquityAlgorithm extends QCAlgorithm {
+
+    @Override
+    public void Initialize() {
+        SetStartDate(2015, 9, 28);
+        SetEndDate(2015, 11, 13);
+        for (String symbol : Symbols.Equity.All) {
+            AddSecurity(SecurityType.wrap(SecurityType.Equity), symbol, Resolution.wrap(Resolution.Minute), true, false);
+        }
+    }
+
+    @Override
+    public void OnData(Slice slice) {
+    }
+}
